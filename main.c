9e5c7b867896058d420a0ffeaeb77360fed1fca6
KunYi Chen <kunyi.chen@gmail.com>
2025-05-15 22:10:17 +0800

    create main.c for project

---
+++ main.c
@@ -0,0 +1,91 @@
+
+#include <stdint.h>
+#include "NUC100Series.h"
+#include "DAP.h"
+
+// 48MHz for USB
+#define PLL_CLOCK	(48000000U)
+
+uint32_t main(void);
+
+static __INLINE SYS_Init(void)
+{
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init System Clock                                                                                       */
+    /*---------------------------------------------------------------------------------------------------------*/
+
+    /* Enable Internal RC 22.1184 MHz clock */
+    CLK_EnableXtalRC(CLK_PWRCON_OSC22M_EN_Msk);
+
+    /* Waiting for Internal RC clock ready */
+    CLK_WaitClockReady(CLK_CLKSTATUS_OSC22M_STB_Msk);
+
+    /* Switch HCLK clock source to Internal RC and HCLK source divide 1 */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLK_S_HIRC, CLK_CLKDIV_HCLK(1));
+
+    /* Enable external XTAL 12 MHz clock */
+    CLK_EnableXtalRC(CLK_PWRCON_XTL12M_EN_Msk);
+
+    /* Waiting for external XTAL clock ready */
+    CLK_WaitClockReady(CLK_CLKSTATUS_XTL12M_STB_Msk);
+
+    /* Set core clock */
+    CLK_SetCoreClock(PLL_CLOCK);
+
+    /* Enable module clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+    CLK_EnableModuleClock(USBD_MODULE);
+
+    /* Select module clock source */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART_S_HXT, CLK_CLKDIV_UART(1));
+    CLK_SetModuleClock(USBD_MODULE, 0, CLK_CLKDIV_USB(1));
+
+
+   /*---------------------------------------------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                                                                 */
+    /*---------------------------------------------------------------------------------------------------------*/
+
+    /* Set GPB multi-function pins for UART0 RXD and TXD, and Clock Output */
+    SYS->GPB_MFP &= ~(SYS_GPB_MFP_PB0_Msk | SYS_GPB_MFP_PB1_Msk | SYS_GPB_MFP_PB12_Msk);
+    SYS->GPB_MFP |= (SYS_GPB_MFP_PB0_UART0_RXD | SYS_GPB_MFP_PB1_UART0_TXD | SYS_GPB_MFP_PB12_CLKO);
+    SYS->ALT_MFP &= ~SYS_ALT_MFP_PB12_Msk;
+    SYS->ALT_MFP |= SYS_ALT_MFP_PB12_CLKO;
+
+    /* Enable CLKO(PB.12) for monitor HCLK. CLKO = HCLK/8 Hz*/
+    // EnableCLKO((2 << CLK_CLKSEL2_FRQDIV_S_Pos), 2);
+}
+
+static __INLINE UART0_Init(void)
+{
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init UART                                                                                               */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Reset IP */
+    SYS->IPRSTC2 |=  SYS_IPRSTC2_UART0_RST_Msk;
+    SYS->IPRSTC2 &= ~SYS_IPRSTC2_UART0_RST_Msk;
+
+    /* Configure UART0 and set UART0 Baudrate */
+    UART0->BAUD = UART_BAUD_MODE2 | UART_BAUD_MODE2_DIVIDER(__HXT, 115200);
+    UART0->LCR = UART_WORD_LEN_8 | UART_PARITY_NONE | UART_STOP_BIT_1;
+
+    /* Enable UART Interrupt */
+    UART0->IER = UART_IER_TOUT_IEN_Msk | UART_IER_RDA_IEN_Msk;
+}
+
+static __INLINE app_main(void) {
+	DAP_Setup();
+
+	while (1) {
+
+	}
+}
+
+uint32_t main(void) {
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+    SYS_Init();
+    UART0_Init();
+
+    app_main();
+    return 0;
+}
