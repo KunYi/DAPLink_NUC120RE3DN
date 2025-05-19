dc575a21a8e96c7c7305fd3234fe8e1aa3e4108b
KunYi Chen <kunyi.chen@gmail.com>
2025-05-19 18:19:12 +0800

    porting osal_threadx for tinyusb, work in progress

---
+++ tinyusb/osal/osal_threadx.h
@@ -0,0 +1,94 @@
+#ifndef TUSB_OSAL_THREADX_H_
+#define TUSB_OSAL_THREADX_H_
+
+// ThreadX header
+#include "tx_api.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//--------------------------------------------------------------------+
+// TASK API
+//--------------------------------------------------------------------+
+#define MS_TO_TICKS(ms) (((ms) * TX_TIMER_TICKS_PER_SECOND + 999) / 1000)
+
+TU_ATTR_ALWAYS_INLINE static inline uint32_t _osal_ms2tick(uint32_t msec) {
+  if ( msec == OSAL_TIMEOUT_WAIT_FOREVER ) return TX_WAIT_FOREVER;
+  if ( msec == 0 ) return 0;
+
+  uint32_t ticks = MS_TO_TICKS(msec);
+
+  if ( ticks == 0 ) ticks = 1;
+
+  return ticks;
+}
+
+TU_ATTR_ALWAYS_INLINE static inline void osal_task_delay(uint32_t msec) {
+  tx_thread_sleep(_osal_ms2tick(msec));
+}
+//--------------------------------------------------------------------+
+// Semaphore API
+//--------------------------------------------------------------------+
+typedef struct TX_SEMAPHORE_STRUCT osal_semaphore_def_t;
+typedef struct TX_SEMAPHORE_STRUCT* osal_semaphore_t;
+
+TU_ATTR_ALWAYS_INLINE static inline
+osal_semaphore_t osal_semaphore_create(osal_semaphore_def_t *semdef) {
+  return (TX_SUCCESS == tx_semaphore_create(semdef, "tusb", 1)) ? semdef : NULL;
+}
+
+TU_ATTR_ALWAYS_INLINE static inline bool osal_semaphore_delete(osal_semaphore_t semd_hdl) {
+  return (TX_SUCCESS == tx_semaphore_delete(semd_hdl));
+}
+
+TU_ATTR_ALWAYS_INLINE static inline bool osal_semaphore_post(osal_semaphore_t sem_hdl, bool in_isr) {
+  (void) in_isr;
+  return (TX_SUCCESS == tx_semaphore_put(sem_hdl));
+}
+
+TU_ATTR_ALWAYS_INLINE static inline bool osal_semaphore_wait(osal_semaphore_t sem_hdl, uint32_t msec) {
+  return (TX_SUCCESS == tx_semaphore_get(sem_hdl, _osal_ms2tick(msec)));
+}
+
+TU_ATTR_ALWAYS_INLINE static inline void osal_semaphore_reset(osal_semaphore_t const sem_hdl) {
+    // TODO: implement
+
+}
+
+//--------------------------------------------------------------------+
+// MUTEX API (priority inheritance)
+//--------------------------------------------------------------------+
+typedef struct TX_MUTEX_STRUCT  osal_mutex_def_t;
+typedef struct TX_MUTEX_STRUCT* osal_mutex_t;
+
+TU_ATTR_ALWAYS_INLINE static inline osal_mutex_t osal_mutex_create(osal_mutex_def_t* mdef) {
+  return (TX_SUCCESS == tx_mutex_create(mdef, "tusb", TX_INHERIT)) ? mdef : NULL;
+}
+
+TU_ATTR_ALWAYS_INLINE static inline bool osal_mutex_delete(osal_mutex_t mutex_hdl) {
+  return (TX_SUCCESS == tx_mutex_delete(mutex_hdl));
+}
+
+TU_ATTR_ALWAYS_INLINE static inline bool osal_mutex_lock(osal_mutex_t mutex_hdl, uint32_t msec) {
+  return (TX_SUCCESS == tx_mutex_get(mutex_hdl,_osal_ms2tick(msec)));
+}
+
+TU_ATTR_ALWAYS_INLINE static inline bool osal_mutex_unlock(osal_mutex_t mutex_hdl) {
+  return (TX_SUCCESS == tx_mutex_put(mutex_hdl));
+}
+
+//--------------------------------------------------------------------+
+// QUEUE API
+//--------------------------------------------------------------------+
+
+
+TU_ATTR_ALWAYS_INLINE static inline osal_queue_t osal_queue_create(osal_queue_def_t* qdef) {
+  return (TX_SUCCESS == tx_queue_create(qdef, "tusb", ))
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
